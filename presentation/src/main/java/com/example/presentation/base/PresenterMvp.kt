4d88e27c4c08a0a37278e0d035551c046c06d230
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-02-27 21:44:57 +0700

    Edit PresenterMvp

--- presentation/src/main/java/com/example/presentation/base/PresenterMvp.kt
+++ presentation/src/main/java/com/example/presentation/base/PresenterMvp.kt
@@ -3,5 +3,31 @@ package com.example.presentation.base
 import java.lang.ref.WeakReference
 
 class PresenterMvp<V : ViewMvp> {
-    private var weakReference : WeakReference<V>? = null
+    private var weakReference: WeakReference<V>? = null
+    fun attachView(view: V) {
+        if (!isViewAttached) {
+            weakReference = WeakReference(view)
+        }
+        onAttachView()
+    }
+
+    private fun onAttachView() {
+
+    }
+
+    private fun detachView() {
+        weakReference?.clear()
+        weakReference = null
+        onDetachView()
+    }
+
+    private fun onDetachView() {
+
+    }
+
+    val view: V?
+        get() = weakReference?.get()
+
+    private val isViewAttached: Boolean
+        get() = weakReference != null && weakReference!!.get() != null
 }
