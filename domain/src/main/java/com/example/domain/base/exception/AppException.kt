869f7f7f91095fa25d341b69b96da0ea11c22243
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-04-22 14:23:45 +0700

    Edit AppException.kt

--- domain/src/main/java/com/example/domain/base/exception/AppException.kt
+++ domain/src/main/java/com/example/domain/base/exception/AppException.kt
@@ -1,12 +1,8 @@
 package com.example.domain.base.exception
 
 
-class AppException(message: String, cause: Throwable?) : Exception(message,cause){
-    var errorCode : Int = 0
-
-    constructor(message: String) : this(message,null)
-
-    constructor(message: String, errorCode: Int) : this(message){
-        this.errorCode = errorCode
-    }
-}
+class AppException(
+    message: String = "",
+    cause: Throwable = Throwable(),
+    errorCode: Int = 0
+) : Exception(message, cause)
